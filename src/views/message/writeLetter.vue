446fc5ef9a7354c087d61720dd640bbf0f28de09
Yvette2001 <87475993+Crestina2001@users.noreply.github.com>
2021-07-16 05:24:24 +0800

    增加写信功能
    增加写信功能

--- src/views/message/writeLetter.vue
+++ src/views/message/writeLetter.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="certificate-page bg-white p-4 rounded">
-    <h3 class="mb-4 color-primary">实名认证</h3>
+    <h3 class="mb-4 color-primary">发起会话</h3>
    <el-form
             :model="ruleForm"
             :rules="rules"
@@ -8,21 +8,31 @@
             label-width="auto"
             class="demo-ruleForm"
           >
-      <el-form-item label="真实姓名" prop="realname">
-        <el-input v-model="ruleForm.realname" prefix-icon="el-icon-user" placeholder="请输入真实姓名" />
+      <el-form-item label="收件人" prop="Receiver.user_name">
+        <el-input v-model="ruleForm.Receiver.user_name" prefix-icon="el-icon-user" placeholder="请输入收件人" />
       </el-form-item>
-      <el-form-item label="身份证号" prop="identity">
-        <el-input v-model="ruleForm.identity" prefix-icon="el-icon-folder-opened" placeholder="请输入身份证号" />
+      <el-form-item label="标 题 " prop="session_type">
+        <el-input v-model="ruleForm.session_type"  
+        placeholder="请输入标题"
+        maxlength="14"
+        show-word-limit 
+        />
       </el-form-item>
-      <el-form-item label="性别">
-        <el-radio-group v-model="ruleForm.sex" prop="sex">
-          <el-radio label="男" />
-          <el-radio label="女" />
-        </el-radio-group>
+      <el-form-item label="内 容" prop="session_detail">
+        <el-input 
+        type="textarea"
+        v-model="ruleForm.session_detail" 
+        prefix-icon="el-icon-folder-opened" 
+        placeholder="请输入内容"
+        maxlength="150"
+        show-word-limit
+         />
       </el-form-item>
       <el-form-item>
         <div class="text-right">
-          <el-button type="primary" @click="onSubmit">提交</el-button>
+          <el-button type="primary"
+          round 
+          @click="onSubmit">提交</el-button>
         </div>
       </el-form-item>
     </el-form>
@@ -36,28 +46,38 @@ export default {
   data() {
     return {
       ruleForm: {
-        realname: '',
-        identity: '',
-        sex: '男'
+        Sender:{
+            user_name:''
+        },
+        Receiver:{
+            user_name:''
+        },
+        session_detail:"",
+        session_type:""
       },
-
        rules: {
-        realname: [
-          { required: true, message: "请输入您的姓名", trigger: "blur" },
+        Receiver:{
+            user_name:[
+          { required: true, message: "请输入收件人", trigger: "blur" },
           
         ],
-         identity: [
-          { required: true, message: "请输入您的身份证号", trigger: "blur" },
+        },
+         session_type: [
+          { required: true, message: "请输入标题", trigger: "blur" },
            {
-            min: 18,
-            max: 18,
-            message: "请认真核对位数",
+            min: 1,
+            max: 14,
             trigger: "blur",
           },
         ],
 
-         sex: [
-          { required: true, message: "请选择性别", trigger: "blur" },
+         session_detail: [
+          { required: true, message: "请选择内容", trigger: "blur" },
+          {
+              min:1,
+              max:150,
+              trigger:"blur"
+          }
         ],
 
         },
@@ -65,16 +85,16 @@ export default {
         
     }
   },
-  methods: {
-    // 实名认证
+  methods: { 
     onSubmit() {
-      this.$confirm('实名认证后无法修改，确定吗?', '提示', {
+      this.$confirm('确定要发送吗?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
         type: 'warning'
       }).then(() => {
-        // 实名认证
-        //verified(data)
+
+        //调用api接口
+        //...
       })
     }
   }
