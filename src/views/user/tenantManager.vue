30681a46fd0b75f3d8204a58da4048bf8a551f7b
Yvette2001 <87475993+Crestina2001@users.noreply.github.com>
2021-07-16 13:12:13 +0800

    租客管理和房东管理样式修改
    解决了信用分框显示不全的问题

--- src/views/user/tenantManager.vue
+++ src/views/user/tenantManager.vue
@@ -7,7 +7,7 @@
             (data) => !search || String(data.user_id).startsWith(search)
           )
         "
-        style="width: 75%"
+        style="width: 90%"
         class="tableData"
       >
         <el-table-column type="expand">
